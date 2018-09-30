059209645020f0c0735eb9795b39c9db0d5155ad
Danila Loginov <danila@loginov.rocks>
2018-09-30 09:17:54 +0300

    chore: update dependencies, v1.3.3

--- src/BluetoothTerminal.js
+++ src/BluetoothTerminal.js
@@ -10,7 +10,7 @@ class BluetoothTerminal {
    * @param {string} [sendSeparator='\n'] - Send separator
    */
   constructor(serviceUuid = 0xFFE0, characteristicUuid = 0xFFE1,
-              receiveSeparator = '\n', sendSeparator = '\n') {
+      receiveSeparator = '\n', sendSeparator = '\n') {
     // Used private variables.
     this._receiveBuffer = ''; // Buffer containing not separated data.
     this._maxCharacteristicValueLength = 20; // Max characteristic value length.
